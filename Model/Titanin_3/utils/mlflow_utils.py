5e6bb9e099a832ecbb5cb876e41156bbb5549ca9
tuananhudu <buiduytuananhudu@gmail.com>
2025-05-19 12:30:05 +0700

    MLflow

--- Model/Titanin_3/utils/mlflow_utils.py
+++ Model/Titanin_3/utils/mlflow_utils.py
@@ -1,14 +1,15 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
-import os
+import numpy as np
 
-def log_confusion_matrix(cm, labels, run_id=None, save_path="confusion_matrix.png"):
+def log_confusion_matrix(cm, labels, model_name="Model", save_path=None):
     plt.figure(figsize=(6, 4))
     sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=labels, yticklabels=labels)
+    plt.ylabel('Actual')
     plt.xlabel('Predicted')
-    plt.ylabel('True')
-    plt.title('Confusion Matrix')
-    plt.tight_layout()
+    plt.title(f"Confusion Matrix - {model_name}")
+    if not save_path:
+        save_path = "confusion_matrix.png"
     plt.savefig(save_path)
     plt.close()
     return save_path
